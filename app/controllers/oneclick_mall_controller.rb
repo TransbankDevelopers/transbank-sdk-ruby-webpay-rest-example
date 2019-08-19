ada1cecc76fa7d7d7e6d28f5567a88c6a38e10ff
Eugenio Nicolás López Ramos <eugenionlopez@gmail.com>
2019-08-18 20:24:08 -0400

    Add mall inscription example

--- app/controllers/oneclick_mall_controller.rb
+++ app/controllers/oneclick_mall_controller.rb
@@ -1,28 +1,28 @@
 class OneclickMallController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def inscription
   end
 
   def start_inscription
     @req = params.as_json
-    @user_name = req['user_name']
-    @email = req['email']
-    @response_url = req['response_url']
+    @user_name = @req['user_name']
+    @email = @req['email']
+    @response_url = @req['response_url']
     session[:user_name] = @user_name
     session[:email] = @email
 
    @resp = Transbank::Webpay::Oneclick::MallInscription::start(user_name: @user_name,
                                                                email: @email,
-                                                               response_url: @response)
+                                                               response_url: @response_url)
    render 'inscription_started'
   end
 
   def finish_inscription
     @req = params.as_json
     @token = @req["TBK_TOKEN"]
-
     @child_commerce_codes = ::Transbank::Webpay::Oneclick::Base::DEFAULT_ONECLICK_MALL_CHILD_COMMERCE_CODES
-
+    @user_name = session[:user_name]
     @resp = Transbank::Webpay::Oneclick::MallInscription::finish(token: @token)
     render 'inscription_finished'
   end
